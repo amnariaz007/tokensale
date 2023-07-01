9e6a63bb323f35113afa5366a2e35a21db4616f9
Cryptogenic <71836791+Cryptogenicc@users.noreply.github.com>
2023-07-01 21:36:12 +0500

    Update erc20withtax.sol

--- erc20withtax.sol
+++ erc20withtax.sol
@@ -3,7 +3,6 @@
 */
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
 // CAUTION
 // This version of SafeMath should only be used with Solidity 0.8 or later,
 // because it relies on the compiler's built in overflow checks.
