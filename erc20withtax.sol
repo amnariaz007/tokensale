1c70621820280d3b53429a8343fd9146b7a39eb1
Cryptogenic <71836791+Cryptogenicc@users.noreply.github.com>
2023-09-06 13:51:52 +0500

    Update erc20withtax.sol

--- erc20withtax.sol
+++ erc20withtax.sol
@@ -7,7 +7,6 @@ pragma solidity ^0.8.0;
 // This version of SafeMath should only be used with Solidity 0.8 or later,
 // because it relies on the compiler's built in overflow checks.
 
-
 /**
  * @dev Wrappers over Solidity's arithmetic operations.
  *
